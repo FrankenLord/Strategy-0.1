a1f8ec08972e929a062cb719df14a9b0c39542ec
unknown <Master>
2024-04-19 11:23:15 +0500

    Надо сделать пчелинно сотовую генерацию

--- Assets/Scripts/GridManagerScript.cs
+++ Assets/Scripts/GridManagerScript.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class GridManagerScript : MonoBehaviour
 {
@@ -17,16 +18,29 @@ public class GridManagerScript : MonoBehaviour
     {
         // Создаем новый пустой GameObject для хранения клеток
         GameObject gridParent = new GameObject("Grid");
-
+        float a;
+        a = cellSize / 2;
         for (int x = 0; x < gridSizeX; x++)
         {
             for (int y = 0; y < gridSizeY; y++)
             {
-                Vector3 position = new Vector3(x * (cellSize + offset), 0, y * (cellSize + offset));
+                if ((y + 1) % 2 == 0)
+                {
+                    Vector3 position = new Vector3((float)(x * cellSize * Math.Sqrt(3) + (cellSize * Math.Sqrt(3) / 2)), 0, (float)(y / 2 * 1.5 * cellSize));
+                    GameObject cell = Instantiate(cellPrefab, position, Quaternion.identity, gridParent.transform);
+                    cell.name = "Cell_" + x + "_" + y;
+                }
+                else
+                {
+                    Vector3 position = new Vector3((float)(x * cellSize * Math.Sqrt(3)), 0, (float)(y * cellSize));
+                    GameObject cell = Instantiate(cellPrefab, position, Quaternion.identity, gridParent.transform);
+                    cell.name = "Cell_" + x + "_" + y;
+                }
+                //Vector3 position = new Vector3(x * (cellSize + offset), 0, y * (cellSize + offset));
 
                 // Создаем клетку из префаба и делаем ее дочерним объектом созданного GameObject "Grid"
-                GameObject cell = Instantiate(cellPrefab, position, Quaternion.identity, gridParent.transform);
-                cell.name = "Cell_" + x + "_" + y;
+                //GameObject cell = Instantiate(cellPrefab, position, Quaternion.identity, gridParent.transform);
+                //cell.name = "Cell_" + x + "_" + y;
                 // Дополнительные действия, если необходимо
             }
         }
